739d7a94f2c901b553f365351435a9e5ac80b60c
maponbie <martha.aponbiede@valpo.edu>
2024-04-29 17:23:41 -0500

    Initial commit

---
+++ src/main/java/maponbie/banking/bankingapp/mapper/AccountMapper.java
@@ -0,0 +1,28 @@
+package maponbie.banking.bankingapp.mapper;
+
+import maponbie.banking.bankingapp.dto.AccountDto;
+import maponbie.banking.bankingapp.entity.Account;
+
+public class AccountMapper {
+
+    public static Account mapToAccount(AccountDto accountDto) {
+        Account account = new Account(
+                accountDto.getId(),
+                accountDto.getAccountHolderName(),
+                accountDto.getBalance()
+        );
+        return account;
+    }
+
+    public static AccountDto mapToAccountDto(Account account){
+        AccountDto accountDto = new AccountDto(
+                account.getId(),
+                account.getAccountHolderName(),
+                account.getBalance()
+        );
+
+        return accountDto;
+    }
+
+
+}
